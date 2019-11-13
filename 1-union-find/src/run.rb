ab675b591e461c898cf3dfaa7a00109ecb7574e5
kowalski <darkkowalski2012@gmail.com>
2019-11-13 20:54:32 +0800

    improve: add benchmark

--- 1-union-find/src/run.rb
+++ 1-union-find/src/run.rb
@@ -1,19 +1,21 @@
 require_relative './UF'
+require 'benchmark'
 
 def run
   n = gets.to_i
   puts "#{n} nodes"
 
   uf = UF.new(n)
-
-  input = ""
-  until input[0] == "quit"
-    input = gets.chomp.to_s.split(' ')
-    p = input[0]
-    q = input[1]
-    next if uf.connected?(p.to_i, q.to_i)
-    uf.union(p, q)
-    puts "#{p} #{q}"
+  bm = Benchmark.measure do
+    input = ""
+    until input[0] == "quit"
+      input = gets.chomp.to_s.split(' ')
+      p = input[0]
+      q = input[1]
+      next if uf.connected?(p.to_i, q.to_i)
+      uf.union(p, q)
+      puts "#{p} #{q}"
+    end
   end
 
   puts "#{uf.count} components"
@@ -21,6 +23,7 @@ def run
   puts "Index: #{(0...n).to_a}"
   puts "ID:    #{uf.id}"
   puts "Size:  #{uf.sz}"
+  puts "BM: #{bm}"
 end
 
 run
