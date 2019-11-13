3aac3ff8fa0e7ea1e5353f866c444f7659d47504
kowalski <darkkowalski2012@gmail.com>
2019-11-13 20:49:09 +0800

    improve: add benchmark

--- 2-selection-sort/src/run.rb
+++ 2-selection-sort/src/run.rb
@@ -1,13 +1,14 @@
 require_relative './Selection'
+require 'benchmark'
 def run
   sl = Selection.new
   arr = gets.chomp.split(' ').to_a.map(&:to_i)
   puts "Origin: #{arr}"
   puts "Answer: #{arr.sort}"
   
-  arr = sl.sort(arr)
+  bm = Benchmark.measure { arr = sl.sort(arr)}
   puts "Sorted: #{arr}"
-  puts "Done!" if sl.sorted?(arr)
+  puts "BM: #{bm}" if sl.sorted?(arr)
 end
 
 run
